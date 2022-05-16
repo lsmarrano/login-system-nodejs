055b470a6b9f8731ca9721b0297fc9a667ca34f6
lsmarrano <76629457+lsmarrano@users.noreply.github.com>
2022-05-16 10:49:23 -0300

    index, login and register config

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 
+const users = []
+
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.render('index.ejs', { name: 'Babs' })
@@ -11,8 +14,12 @@ app.get('/login', (req, res) => {
   res.render('login.ejs')
 })
 
+app.post('/login', (req, res) => {})
+
 app.get('/register', (req, res) => {
   res.render('register.ejs')
 })
+
+app.post('/register', (req, res) => {})
 
 app.listen(3000)
