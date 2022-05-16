f3558c827414c06364a414abd5b8013ca3e2a6ef
lsmarrano <76629457+lsmarrano@users.noreply.github.com>
2022-05-16 12:49:49 -0300

    add and init config of express-session and express-flash

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+
+const initializePassport = require('./passport.initialize')
+initializePassport(passport, (email) => {
+  return users.find((user) => user.email === email)
+})
 
 const users = []
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', (req, res) => {
   res.render('index.ejs', { name: 'Babs' })
